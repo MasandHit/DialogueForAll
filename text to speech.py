bb5fff8d72261e0c31eb64e87ec611e712f3c11e
MasandHit <hitendrasinghmasand@gmail.com>
2024-11-29 01:22:55 -0700

    Add files via upload

---
+++ text to speech.py
@@ -0,0 +1,14 @@
+from gtts import gTTS
+import os
+
+def generate_audio(text):
+    tts = gTTS(text=text, lang='en')
+    tts.save("output.mp3")
+    os.system("afplay output.mp3") 
+    os.remove("output.mp3") 
+
+
+generate_audio()
+
+
+
